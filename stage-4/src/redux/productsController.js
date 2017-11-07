c8d4e3b31623727f55ad2b815d54ba366afbee36
ericdrewes <eric.d.drewes@gmail.com>
2017-11-07 14:59:37 -0600

    stage-4 complete

--- stage-4/src/redux/productsController.js
+++ stage-4/src/redux/productsController.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const getAllProducts = function() {
-    return axios.get("https://practiceapi.devmountain.com/products/")
-    .then(response => response.data);
-}
+export const getAllProducts = function() {
+	return axios.get('https://practiceapi.devmountain.com/products/').then(response => response.data);
+};
