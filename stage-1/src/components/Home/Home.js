e6f2a188093e7f60a4dff9cb5b946c4b6394efcb
ericdrewes <eric.d.drewes@gmail.com>
2018-02-24 18:50:06 -0600

    stage-1 update

--- stage-1/src/components/Home/Home.js
+++ stage-1/src/components/Home/Home.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+
+import React, {Component} from 'react'
 import './Home.css';
 
 class Home extends Component {
-	render() {
-		return <div className="home-landing" />;
-	}
+    render() {
+        return (
+            <div className="home-landing"></div>
+        )
+    }
 }
 
 export default Home;
